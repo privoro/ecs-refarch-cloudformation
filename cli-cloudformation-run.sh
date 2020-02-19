2a0d400f1f0bef0ee951fe87b34873da2595b1a8
Aaron Dowdy <dowdy.aaron@gmail.com>
2020-02-19 11:45:36 -0700

    template runs demo on our vpc

--- cli-cloudformation-run.sh
+++ cli-cloudformation-run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+# USE AWS CLI TO CREATE A STACK FROM CLOUDFORMATION
+
 aws cloudformation create-stack --stack-name=$1 \
 --template-body file://$2 \
 --parameters \
--- dev.sh
+++ dev.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -ex
 
+# DEV WORKFLOW FOR STACK CREATION + TEST
+
 ./cli-cloudformation-run.sh prom-test-$(date '+%a-%m-%d-%H%M') prometheus-cf.yml
