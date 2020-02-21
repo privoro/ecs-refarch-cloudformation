adc49d316e88db714769f79809d9706ca4695f93
Aaron Dowdy <dowdy.aaron@gmail.com>
2020-02-20 17:16:52 -0700

    cleanup, rename stacks

--- dev.sh
+++ dev.sh
@@ -3,4 +3,4 @@ set -ex
 
 # DEV WORKFLOW FOR STACK CREATION + TEST
 
-./cli-cloudformation-run.sh prom-test-$(date '+%a-%m-%d-%H%M') prometheus-cf.yml
+./cli-cloudformation-run.sh prometheus-ecs-$(date '+%a-%m-%d-%H%M') prometheus-cf.yml
